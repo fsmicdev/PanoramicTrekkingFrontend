b7b2eace66699e0eca7c7ab79956b16bd0299031
Mic G <fs.micdev@gmail.com>
2019-09-14 19:21:20 +1000

    Minor improvements to little example Angular Material Card ( from Angular Material website showcase ).

--- panoramic-trekking-frontend/src/app/app.component.ts
+++ panoramic-trekking-frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {MatIconRegistry} from "@angular/material/icon";
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'panoramic-trekking-frontend';
+
+  title = 'Panoramic Trekking';
+
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon(
+      'facebook-share',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/images/facebook.svg'));
+    // Icon attained from https://www.flaticon.com/free-icon/facebook_174848
+    // Icon made by [author link] from www.flaticon.com
+    // Attribution style:
+    // <p class="wysiwyg-text-align-center"><strong><em>Icon made by&nbsp;
+    // </em></strong><a href="https://www.freepik.com/home"><strong><em>Freepik</em></strong></a><strong>
+    // <em>&nbsp;from&nbsp;</em></strong><strong><em>
+    // <a href="http://www.flaticon.com/">www.flaticon.com</a></em></strong>
+    // </p>
+  }
 }
