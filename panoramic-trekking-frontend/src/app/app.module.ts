a9e9714a7f62f5d3b6d3e01ee32bd90ad9cdeefa
Mic G <fs.micdev@gmail.com>
2019-09-15 18:21:04 +1000

    Improvements to Add Photo view, and moved common styles to styles.css.

--- panoramic-trekking-frontend/src/app/app.module.ts
+++ panoramic-trekking-frontend/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {
   MatToolbarModule,
   MatCardModule,
@@ -17,11 +17,11 @@ import { AddPhotoComponent } from './add-photo/add-photo.component';
 import { MatFileUploadModule } from 'angular-material-fileupload';
 import { ViewPhotoDetailsComponent } from './view-photo-details/view-photo-details.component';
 
-import {MatProgressBarModule} from '@angular/material/progress-bar';
-import {HttpClientModule} from '@angular/common/http';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatNativeDateModule} from '@angular/material/core';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { HttpClientModule } from '@angular/common/http';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [
